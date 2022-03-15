f71dcac7c8da427b51a50ef926da5202f163b9ba
rixeli <vimal.kr.pal@gmail.com>
2022-03-15 07:39:44 +0530

    house robber

--- src/main/java/com/algo/dp/HouseRobber.java
+++ src/main/java/com/algo/dp/HouseRobber.java
@@ -2,19 +2,25 @@ package com.algo.dp;
 
 public class HouseRobber {
 
+    public static void main(String[] args) {
+        HouseRobber robber = new HouseRobber();
+        int[] nums = {1, 2, 3};
+        System.out.println(robber.rob(nums));
+    }
+
     public int rob(int[] nums) {
         int l = nums.length;
-        if(l==0){
+        if (l == 0) {
             return 0;
         }
-        if(l==1){
+        if (l == 1) {
             return nums[0];
         }
-        if(l==2){
-            return Math.max(nums[0],nums[1]);
+        if (l == 2) {
+            return Math.max(nums[0], nums[1]);
         }
-        if(l==3){
-            return Math.max(nums[0]+nums[2], nums[1]);
+        if (l == 3) {
+            return Math.max(nums[0] + nums[2], nums[1]);
         }
         int sums[] = new int[l];
         sums[0] = nums[0];
